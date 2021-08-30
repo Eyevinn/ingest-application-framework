51af20c0c63fb7c0b32b53e7818864dc5722c057
Fredrik Lundkvist <fredrik.lundkvist@eyevinn.se>
2021-08-30 09:16:54 +0200

    remove implementation interfaces, fix typo

--- interfaces/interfaces.d.ts
+++ interfaces/interfaces.d.ts
@@ -4,26 +4,9 @@ import winston from "winston";
 interface IafUploadModule {
     logger: winston.Logger;
     onFileAdd(filePath: string, readStream: Readable): any;
-    uploader?: Uploader;
-    dispatcher?: TranscodeDispatcher;
 }
 
-interface Uploader {
-    destination: string;
-    logger: winston.Logger;
-    upload(fileStream: Readable, fileName: string);
-}
-
-interface TranscodeDispatcher {
-    encodeParams: any,
-    inputLocation: string;
-    outputDestination: string;
-    logger: winston.Logger;
-    dispatch(fileName: string): Promise<any>;
-
-}
-
-interface IafFileWathModule {
+interface IafFileWatchModule {
     fileInput: string;
     logger: winston.Logger;
     onAdd(callback: (filePath: string, readStream: Readable) => any);
